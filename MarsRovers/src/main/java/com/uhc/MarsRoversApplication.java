5b429b813df2e17c9908a977c4e9b6d24ee2a784
uhconst <uryelhenrique.c@gmail.com>
2017-05-23 08:37:50 -0300

    Criando projeto MarsRovers

---
+++ MarsRovers/src/main/java/com/uhc/MarsRoversApplication.java
@@ -0,0 +1,12 @@
+package com.uhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoversApplication.class, args);
+	}
+}
---
+++ MarsRovers/src/test/java/com/uhc/MarsRoversApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uhc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarsRoversApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
